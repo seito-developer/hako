d3da12f476dfdbc606a6c3da23f1d560d9a464f9
seito <seitokeri03@yahoo.co.jp>
2020-05-16 16:35:35 +0900

    switched to scss

--- nextjs-app/components/button.module.js
+++ nextjs-app/components/button.module.js
@@ -1,6 +1,6 @@
-import styles from './button.module.css'
+import styles from './button.module.scss'
 
 export default function Button({ children }) {
 	return <button className={styles.primary} type="button">{children}</button>
 }
-
+ 
