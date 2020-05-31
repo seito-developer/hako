ac878b2ed4588c6856dbb686b518533d05109351
seito <seitokeri03@yahoo.co.jp>
2020-05-31 23:48:26 +0900

    updated minimum signup

---
+++ nextjs-app/components/hero.module.js
@@ -0,0 +1,26 @@
+import stylesHero from './hero.module.scss'
+import stylesLayout from './layout.module.scss'
+import cn from 'classnames'
+import Button from '../components/button.module.js'
+
+export default function Hero() {
+	return (
+		<div className={stylesHero.hero}>
+			<div className={stylesLayout.container}>
+				<div className={stylesHero.hero__logo}></div>
+				<div className={stylesHero.hero__headline}>BindHub</div>
+				<h1 className={stylesHero.hero__desc}>
+					Help Your project.<br/>
+					It's easy to access to any materials here.
+				</h1>
+				<div className={stylesHero.hero__button}>
+					<Button color="google" size="large">
+						{/* <FontAwesomeIcon icon={faGoogle} className={classNames(styles.icon, styles.large, styles.google)} /> */}
+						Login with Google
+					</Button>
+				</div>
+			</div>
+		</div>
+		)
+}
+ 
